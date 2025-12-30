240beed9af818079dd99b6d032aba52eb01be202
hasti_phd <hpirhadi@yahoo.com>
2025-12-30 14:57:37 +0330

    finally

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const inputOne= document.getElementById("num1");
+const inputOne = document.getElementById("num1");
 const inputTwo = document.getElementById("num2");
 const calculator = document.getElementById("calculation");
 const result = document.getElementById("sum");
 
-calculator.addEventListener("click",()=> {
-   const getValueInputOne=inputOne.value;
-    const getValueInputTwo = inputTwo.value;
-    // const totalvalue= getValueInputOne*(1+getValueInputTwo/100);
-    console.log (result,"sum")
-    result.textContent = getValueInputOne * (1 + getValueInputTwo / 100);
-})
+console.log(num1);
+console.log(num2);
+console.log(calculator);
+console.log(result, "sum");
+calculator.addEventListener("click", () => {
+  const getValueInputOne = Number(inputOne.value);
+  const getValueInputTwo = Number(inputTwo.value);
+  // const totalvalue= getValueInputOne*(1+getValueInputTwo/100);
+  result.textContent = getValueInputOne * (1 + getValueInputTwo / 100);
+});
